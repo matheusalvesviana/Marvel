0c96e1f3d3f9e316a92fb3a9f3a2292ebc3d0920
Matheus Alves Viana <theus_a7x@hotmail.com>
2020-06-21 19:38:58 -0300

    Start Koin

--- app/src/main/java/com/example/marvel/MarvelApplication.kt
+++ app/src/main/java/com/example/marvel/MarvelApplication.kt
@@ -2,11 +2,18 @@ package com.example.marvel
 
 import android.app.Application
 import com.example.marvel.di.InjectionImpl
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.loadKoinModules
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MarvelApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidContext(applicationContext)
+            printLogger(Level.DEBUG)
+        }
         val injection = InjectionImpl()
 
         loadKoinModules(injection.module)
